e151789d93fa2472a221f2795a807e217b29c20b
Muntaser Muttaqi <muntasermuttaqi@gmail.com>
2023-06-30 02:00:45 +0600

    Profile update count notification

---
+++ app/Notifications/Admin/ProfileUpdateCountNotification.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\Notifications\Admin;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Notifications\Notification;
+
+class ProfileUpdateCountNotification extends Notification
+{
+    use Queueable;
+
+    /**
+     * Create a new notification instance.
+     */
+    public $count;
+
+    public function __construct($count)
+    {
+        $this->count = $count;
+    }
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @return array<int, string>
+     */
+    public function via(object $notifiable): array
+    {
+        return ['mail'];
+    }
+
+    /**
+     * Get the mail representation of the notification.
+     */
+    public function toMail(object $notifiable): MailMessage
+    {
+        return (new MailMessage)
+            ->subject('Action Required: Unread Notifications')
+            ->greeting('Unread Notifications')
+            ->line('Dear ' . $notifiable->name . ',')
+            ->line('You have **' . $this->count . '** unread notifications on ***' . config('app.name') . '*** today that require your attention. Please take a moment to review and respond to them.')
+            ->line('To ensure a smooth user experience and maintain the security of our platform, we kindly request your prompt action. Here are the details:')
+            ->line('- Log in to ***' . config('app.name') . '*** to access the notification center and view the unread notifications.')
+            ->line('- Review each notification carefully and take appropriate actions based on their importance and urgency.')
+            ->line('- If any notifications require further investigation or response, please prioritize them accordingly.')
+            ->line('If you have any questions or concerns regarding the notifications, feel free to reach out to developer team.')
+            ->salutation('Thank you for your dedication to providing excellent user support and maintaining the integrity of our platform.');
+    }
+
+    /**
+     * Get the array representation of the notification.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(object $notifiable): array
+    {
+        return [
+            //
+        ];
+    }
+}
--- routes/console.php
+++ routes/console.php
@@ -1,7 +1,11 @@
 <?php
 
+use App\Models\Role;
+use App\Models\User;
+use App\Notifications\Admin\ProfileUpdateCountNotification;
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Notification;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,3 +21,31 @@ use Illuminate\Support\Facades\Artisan;
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
+
+Artisan::command('admin:profile-update-count-notification', function () {
+    try {
+        $admins = User::with('role')
+            ->has('role')
+            ->whereHas('role', function ($query) {
+                return $query->whereIn('slug', Role::slugsInArray('admin'));
+            })
+            ->get();
+
+        $this->withProgressBar($admins, function ($admin) {
+            $notificationCount = $admin->notifications()
+                ->where('read_at', null)
+                ->whereDate('created_at', today())
+                ->count();
+
+            if ($notificationCount) {
+                $this->comment(' sending...');
+                $admin->notify((new ProfileUpdateCountNotification($notificationCount)));
+                $this->info(' sent!');
+            }
+            sleep(10);
+        });
+    } catch (\Throwable $th) {
+        logger('admin:profile-update-count-notification', [$th]);
+        throw $th;
+    }
+})->purpose('Admin Profile Update Count Notification');
