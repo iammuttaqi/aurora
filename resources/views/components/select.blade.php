21de35b608233cd7f01c725c8144c4c44794397e
Muntaser Muttaqi <muntasermuttaqi@gmail.com>
2023-10-07 02:37:16 +0600

    inputs classes change

--- resources/views/components/select.blade.php
+++ resources/views/components/select.blade.php
@@ -1,7 +1,7 @@
 @props(['options' => [], 'selected' => null, 'key' => 'id', 'value_key' => 'title', 'default' => 'Select...', 'disabled' => false])
 
 <select {{ $disabled ? 'disabled' : '' }} {!! $attributes->merge([
-    'class' => 'border-gray-300 dark:border-gray-700 dark:bg-gray-900 dark:text-gray-300 focus:border-indigo-500 dark:focus:border-indigo-600 focus:ring-indigo-500 dark:focus:ring-indigo-600 rounded-md shadow-sm block w-full disabled:dark:bg-gray-700 disabled:cursor-not-allowed',
+    'class' => 'py-3 px-4 block w-full border-gray-200 rounded-md text-sm focus:border-blue-500 focus:ring-blue-500 dark:bg-slate-900 dark:border-gray-700 dark:text-gray-400 transition-all disabled:opacity-50 disabled:cursor-not-allowed',
 ]) !!}>
     <option disabled selected value="">{{ $default }}</option>
     @foreach ($options as $option)
--- vite.config.js
+++ vite.config.js
@@ -2,13 +2,13 @@ import laravel from "laravel-vite-plugin";
 import { defineConfig } from "vite";
 
 export default defineConfig({
+    server: {
+        hmr: false,
+    },
     plugins: [
         laravel({
             input: ["resources/css/app.css", "resources/js/app.js"],
-            // refresh: [
-            //     ...refreshPaths,
-            //     'app/Http/Livewire/**',
-            // ],
+            // refresh: [...refreshPaths, "app/Http/Livewire/**"],
             refresh: false,
         }),
     ],
